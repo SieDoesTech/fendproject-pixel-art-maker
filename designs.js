45c4a796ab60b2fcaa90df9a0a3c59ed8db9f948
Sie <siedoestech@gmail.com>
2022-05-17 22:00:32 -0400

    Updated Pixel Canvas
    This edit includes the third expectation of pressing submit to create a brand new grid.

--- designs.js
+++ designs.js
@@ -21,13 +21,18 @@ inputSubmit.addEventListener("click", e => {
 var pixelCanvas="";
 
 // get height and width values
-function makeGrid(){
+function makeGrid(height,width){
     var height = inputHeight.value;
     var width = inputWidth.value;
+
+canvas.innerHTML='';
 
 // Draw rows across
 
 var grid = "";
+
+//   ('makeGrid').remove();
+
     for (var i=0; i<height; i++){
             var row = canvas.insertRow(i);
         // grid+= '<tr id = "pixelRow">'; 
@@ -37,23 +42,26 @@ var grid = "";
             var cell = row.insertCell(j);
             // grid+= '<td id = "pixelCell"> </td>';
              cell.addEventListener('click', colorIn);
+             
         }
         canvas.appendChild(row);
+         pixelCanvas.remove;
         // row.appendChild(cell);
         // grid += '</tr>';
     }
-
 // insert all of it in HTML
 // canvas.innerHTML=grid;
+
+
+
 }
 
  function colorIn (e){
+    e.preventDefault();
     e.target.style.backgroundColor=inputColor.value;
  }
 
 
-
- 
 // input color into cells
 
 // function colorCell() {
